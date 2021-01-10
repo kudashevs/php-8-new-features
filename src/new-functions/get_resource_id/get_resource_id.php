bc44725f0b22118d6aadf68079f09dee11117871
Sergey Kudashev <kudashevs@gmail.com>
2021-01-10 19:19:14 +0300

    Refactor get_resource_id() function example with error generation.

--- src/new-functions/get_resource_id/get_resource_id.php
+++ src/new-functions/get_resource_id/get_resource_id.php
@@ -3,7 +3,7 @@
 const DUMMY_FILE = __DIR__ . '/dummy.txt';
 
 if (false === file_exists(DUMMY_FILE)) {
-    die('File ' . DUMMY_FILE . ' doesn\'t exist. Create file and then try again');
+    trigger_error('File ' . DUMMY_FILE . ' doesn\'t exist. Create file and then try again', E_USER_ERROR);
 }
 
 $handle = fopen(DUMMY_FILE, 'rb');
