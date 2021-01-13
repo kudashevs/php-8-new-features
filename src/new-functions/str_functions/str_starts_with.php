d7f757ed2546c44b20ff022c4e4ae98ea023404c
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 12:14:50 +0300

    Refactor str_starts_with() function example change trigger_error to assert.

--- src/new-functions/str_functions/str_starts_with.php
+++ src/new-functions/str_functions/str_starts_with.php
@@ -5,8 +5,6 @@ $needle_at_the_start = 'the';
 
 $found = str_starts_with($haystack, $needle_at_the_start);
 
-if (true === $found) {
-    echo 'Needle "' . $needle_at_the_start . '" was found at the beginning of the haystack.' . PHP_EOL;
-} else {
-    trigger_error('Needle "' . $needle_at_the_start . '"" must exist at the beginning of the haystack.', E_USER_ERROR);
-}
+assert(true === $found, 'Needle "' . $needle_at_the_start . '"" must exist at the beginning of the haystack.');
+
+echo 'Needle "' . $needle_at_the_start . '" was found at the beginning of the haystack.' . PHP_EOL;
