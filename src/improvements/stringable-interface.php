d908b152cdc81ba2fe93ee9eda625ecdc4b97f11
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 10:48:32 +0300

    Refactor Stringable interface example change trigger_error to assert.

--- src/improvements/stringable-interface.php
+++ src/improvements/stringable-interface.php
@@ -15,8 +15,9 @@ $objectImplementingToString = new Foo();
 /*
  * Any class which implements the __toString() method implements the Stringable interface automatically.
  */
-if (in_array('Stringable', class_implements($objectImplementingToString), true)) {
-    echo 'An instance of "' . $objectImplementingToString::class . '" implements the Stringable interface.' . PHP_EOL;
-} else {
-    trigger_error('The Stringable interface must be implemented on ' . $objectImplementingToString::class . ' instance.', E_USER_ERROR);
-}
+assert(
+    in_array('Stringable', class_implements($objectImplementingToString), true),
+    'The class ' . $objectImplementingToString::class . ' must implement the Stringable interface.'
+);
+
+echo 'An instance of "' . $objectImplementingToString::class . '" implements the Stringable interface.' . PHP_EOL;
