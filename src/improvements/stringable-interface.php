bae1d68ec2cdece05de885d3203238ce1e652249
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 10:44:51 +0300

    Refactor Stringable interface example rename for better readability.

--- src/improvements/stringable-interface.php
+++ src/improvements/stringable-interface.php
@@ -10,13 +10,13 @@ class Foo
     }
 }
 
-$stringable = new Foo();
+$objectImplementingToString = new Foo();
 
 /*
- * Any class which implements a __toString() method implements the Stringable interface automatically.
+ * Any class which implements the __toString() method implements the Stringable interface automatically.
  */
-if (in_array('Stringable', class_implements($stringable), true)) {
-    echo 'An instance of "' . $stringable::class . '" implements the Stringable interface.' . PHP_EOL;
+if (in_array('Stringable', class_implements($objectImplementingToString), true)) {
+    echo 'An instance of "' . $objectImplementingToString::class . '" implements the Stringable interface.' . PHP_EOL;
 } else {
-    trigger_error('The Stringable interface must be implemented on ' . $stringable::class . ' instance.', E_USER_ERROR);
+    trigger_error('The Stringable interface must be implemented on ' . $objectImplementingToString::class . ' instance.', E_USER_ERROR);
 }
