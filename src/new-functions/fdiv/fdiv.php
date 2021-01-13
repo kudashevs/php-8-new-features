c8678d1e759c111246a24097bc6461e99df89ffa
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 12:46:45 +0300

    Refactor fdiv() function example change trigger_error to assert.

--- src/new-functions/fdiv/fdiv.php
+++ src/new-functions/fdiv/fdiv.php
@@ -5,8 +5,6 @@
  */
 $remainder = fdiv(2, 0);
 
-if (INF === $remainder) {
-    echo 'The division result is "' . (string)$remainder . '".' . PHP_EOL;
-} else {
-    trigger_error('The division result "' . (string)$remainder . '" is not INF.', E_USER_ERROR);
-}
+assert(INF === $remainder, 'The division result "' . (string)$remainder . '" is not INF.');
+
+echo 'The division result is "' . (string)$remainder . '".' . PHP_EOL;
