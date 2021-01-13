712b54a7931a5390912b67cd02efcd67815608ce
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 12:45:13 +0300

    Style fdiv() function example update information in the comment.

--- src/new-functions/fdiv/fdiv.php
+++ src/new-functions/fdiv/fdiv.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * According to IEEE-754 standard on Floating-Point Arithmetic fdiv() function will treat
- * division by zero as legal operation. It returns NAN or ±INF mandated by IEEE-754.
+ * According to IEEE-754 standard on Floating-Point Arithmetic the fdiv() function will treat
+ * division by zero as legal operation. The function returns NAN or ±INF mandated by IEEE-754.
  */
 $remainder = fdiv(2, 0);
 
