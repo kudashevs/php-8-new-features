4980365fc6be644506ab163f7ae0081a5f367501
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 11:09:50 +0300

    Refactor Named arguments example shorten the code for better demonstrativeness.

--- src/new-features/named-arguments/named-arguments-inconsistency.php
+++ src/new-features/named-arguments/named-arguments-inconsistency.php
@@ -2,20 +2,13 @@
 /**
  * Artificial example on how to solve array functions inconsistency with named arguments.
  */
-
 $sequence = [
     1, 2, 3, 4, 5
 ];
 
-$mapped = array_map(array: $sequence, callback: function ($v)
-{
-    return $v * 3;
-});
+$mapped = array_map(array: $sequence, callback: fn($v) => $v * 3);
 
-$filtered = array_filter(array: $mapped, callback: function ($v)
-{
-    return $v < 10;
-});
+$filtered = array_filter(array: $mapped, callback: fn($v) => $v < 10);
 
 if (3 === count($filtered)) {
     echo 'The sequence was processed as expected.' . PHP_EOL;
