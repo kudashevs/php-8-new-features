199fd40d77784b73f8ce15db6166821f4e48fd04
Sergey Kudashev <kudashevs@gmail.com>
2021-01-13 12:17:15 +0300

    Refactor str_ends_with() function example rename for better consistency.

--- src/new-functions/str_functions/str_ends_with.php
+++ src/new-functions/str_functions/str_ends_with.php
@@ -1,10 +1,10 @@
 <?php
 
 $haystack = 'the quick brown fox jumps over the lazy dog';
-$needle_at_the_end = 'dog';
+$needleAtTheEnd = 'dog';
 
-$found = str_ends_with($haystack, $needle_at_the_end);
+$found = str_ends_with($haystack, $needleAtTheEnd);
 
-assert(true === $found, 'Needle "' . $needle_at_the_end . '"" must exist at the end of the haystack.');
+assert(true === $found, 'Needle "' . $needleAtTheEnd . '"" must exist at the end of the haystack.');
 
-echo 'Needle "' . $needle_at_the_end . '" was found at the end of the haystack.' . PHP_EOL;
+echo 'Needle "' . $needleAtTheEnd . '" was found at the end of the haystack.' . PHP_EOL;
