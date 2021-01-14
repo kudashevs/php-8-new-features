5e7886c6bb7ee847441d4c87965d4f10179b2516
Sergey Kudashev <kudashevs@gmail.com>
2021-01-14 11:09:31 +0300

    Refactor Constructor property promotion example add default parameter check.

--- src/new-features/property-promotion/property-promotion.php
+++ src/new-features/property-promotion/property-promotion.php
@@ -2,7 +2,7 @@
 
 class Video
 {
-    public function __construct(protected string $title, protected int $duration, protected string $status = 'active')
+    public function __construct(protected string $title, protected int $duration, protected int $status = 1)
     {
         assert(!empty($title), 'The title should not be empty.');
         assert($duration > 0, 'The duration must be greater than 0.');
@@ -17,6 +17,11 @@ class Video
     {
         return $this->duration;
     }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
 }
 
 $title = 'New video';
@@ -25,5 +30,6 @@ $video = new Video($title, $duration);
 
 assert($title === $video->getTitle(), 'The title property was not set as expected.');
 assert($duration === $video->getRawDuration(),'The duration property was not set as expected.');
+assert(1 === $video->getStatus(), 'The status property was not set as expected.');
 
 echo 'The parameters of the ' . $video::class . ' object were set as expected.' . PHP_EOL;
