d8abd3da5073aec524f161c774b86b9cbd30d638
Sergey Kudashev <kudashevs@gmail.com>
2021-01-20 00:34:19 +0300

    Refactor test file add executed files counter and the additional check on its base.

--- tests/test.php
+++ tests/test.php
@@ -4,6 +4,7 @@ define('SOURCE_FOLDER', dirname(__DIR__) . '/src/');
 
 $files = glob(SOURCE_FOLDER . '{,*,*/*,*/*/*,*/*/*/*}.php', GLOB_BRACE);
 
+$executed = 0;
 foreach ($files as $file) {
     $output = null; $code = null;
     exec('php ' . $file, $output, $code);
@@ -12,6 +13,13 @@ foreach ($files as $file) {
         echo 'File ' . $file . ' stopped execution with error ' . $code . '.' . PHP_EOL;
         exit(1);
     }
+
+    ++$executed;
+}
+
+if ($executed !== count($files)) {
+    echo 'The number of files found does not match the number of executed files.'. PHP_EOL;
+    exit(1);
 }
 
 echo 'All example files were executed successfully.' . PHP_EOL;
