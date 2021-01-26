d685dbba19e70889ad637ec5afc6f4724be008ff
Sergey Kudashev <kudashevs@gmail.com>
2021-01-26 22:16:44 +0300

    Refactor return static type example change instanceof to get_class.

--- src/improvements/return-static.php
+++ src/improvements/return-static.php
@@ -17,8 +17,8 @@ class Bar extends Foo
 {
 }
 
-assert(true === Bar::revealSelf() instanceof Foo, 'The revealSelf() method must return a Foo instance.');
+assert(Foo::class === get_class(Bar::revealSelf()), 'The revealSelf() method must return a Foo instance.');
 echo 'The revealSelf() method returned an instance of "' . Foo::class . '" as expected.' . PHP_EOL;
 
-assert(true === Bar::revealStatic() instanceof Bar, 'The revealStatic() method must return a Bar instance.');
+assert(Bar::class === get_class(Bar::revealStatic()), 'The revealStatic() method must return a Bar instance.');
 echo 'The revealStatic() method returned an instance of "' . Bar::class . '" as expected.' . PHP_EOL;
