1c47f1585cedd7559950cca449e21cf9d920648f
Sergey Kudashev <kudashevs@gmail.com>
2021-01-28 11:13:42 +0300

    Refactor test file update output to use streams.

--- tests/test.php
+++ tests/test.php
@@ -9,14 +9,14 @@ $files = glob(SOURCE_FOLDER . '{,*,*/*,*/*/*,*/*/*/*}.php', GLOB_BRACE);
 $executed = 0;
 foreach ($files as $file) {
     if (VERBOSE) {
-        echo $file . PHP_EOL;
+        fwrite(STDOUT, $file . PHP_EOL);
     }
 
     $output = null; $code = null;
     exec('php ' . $file, $output, $code);
 
     if ($code !== 0) {
-        echo 'File ' . $file . ' stopped execution with error ' . $code . '.' . PHP_EOL;
+        fwrite(STDERR, sprintf('File %s stopped execution with error %s.' . PHP_EOL, $file, $code));
         exit(1);
     }
 
@@ -24,9 +24,9 @@ foreach ($files as $file) {
 }
 
 if ($executed !== count($files)) {
-    echo 'The number of files found does not match the number of executed files.'. PHP_EOL;
+    fwrite(STDERR, 'The number of files found does not match the number of executed files.'. PHP_EOL);
     exit(1);
 }
 
-echo 'All ' . $executed . ' example files were successfully executed.' . PHP_EOL;
+fwrite(STDOUT, sprintf('All %s example files were successfully executed.' . PHP_EOL, $executed));
 exit(0);
